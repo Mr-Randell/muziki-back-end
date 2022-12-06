f08e26fd8813836410bacd677a11359d4977f489
kevin-lomenen <kevin.lomenen@student.moringaschool.com>
2022-12-06 22:06:40 +0300

    added show

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,6 +2,15 @@ class ArtistsController < ApplicationController
     def index
         render json: Artist.all, status: :ok
     end
+
+    def show
+        artist = Artist.find_by(id:params[:id])
+        if artist
+            render json: artist, status: :ok
+        else 
+            render json: {error: "Artist not found"}, status: :not_found
+        end
+    end
 end
 
 
