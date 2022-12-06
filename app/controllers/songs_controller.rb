72651381965753cb731ec9689816452d8f5111b9
kevin-lomenen <kevin.lomenen@student.moringaschool.com>
2022-12-07 01:49:48 +0300

    setting the song cruds functions for and its done apart from post which is posting with null id

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,50 @@
 class SongsController < ApplicationController
+        wrap_parameters format: []
+        def index
+            render json: Song.all, status: :ok
+        end
+    
+        def show
+            song = Song.find_by(id:params[:id])
+            if song
+                render json: song, status: :ok
+            else 
+                render json: {error: "Song not found"}, status: :not_found
+            end
+        end
+    
+        def create
+            song = Song.create(song_params)
+            render json: song, status: :created
+        end
+    
+        def update
+            # find
+            song = Song.find_by(id: params[:id])
+            if song
+                #update 
+                song.update(song_params)
+                render json: song, status: :accepted
+            else
+                render json: {error: "Song not found"}, status: :not_found
+            end
+        end
+    
+        def destroy
+             # find
+             song = Song.find_by(id: params[:id])
+             if song
+                song.destroy
+                head :no_content
+             else
+                render json: {error: "Song not found"}, status: :not_found
+             end
+        end
+    
+        private
+    
+        def song_params
+            params.permit(:name, :genre, :year_of_release)
+        end
 end
+    
