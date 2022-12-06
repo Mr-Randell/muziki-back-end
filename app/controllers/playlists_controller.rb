8d9a648e313b17a9f024b2c1d1b825725e19b84a
dennismuturi <dennismuturi1@gmail.com>
2022-12-06 20:06:22 +0300

    added GET request for a single Playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,15 @@
 class PlaylistsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_no_record_response
     def index
         playlists=Playlist.all
         render json: playlists, status: :ok
+    end
+    def show
+        playlist=Playlist.find(params[:id])
+        render json: playlist, status: :ok
+    end
+    private
+    def render_no_record_response
+        render json: {error: "Playlist Not Found"}, status: :not_found
     end
 end
