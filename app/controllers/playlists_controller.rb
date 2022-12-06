e33065f595fd0512379b3dbe82d85c5c2145a841
dennismuturi <dennismuturi1@gmail.com>
2022-12-06 20:21:18 +0300

    added POST request to create a Playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -8,8 +8,21 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_no_record_response
         playlist=Playlist.find(params[:id])
         render json: playlist, status: :ok
     end
+
+    def create
+        
+        playlist=Playlist.create(playlist_params)
+        render json: playlist, status: :created
+    
+    rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
     private
     def render_no_record_response
         render json: {error: "Playlist Not Found"}, status: :not_found
+    end
+    def playlist_params
+        params.permit(:name, :description)
     end
 end
